3660aab7cd4d8634fdc599cfb2c9dd1647a9bc24
sergio vieira <sergiosvieira@gmail.com>
2020-08-19 11:57:42 -0300

    Simplified version o dijkstra algorithm

--- main.cpp
+++ main.cpp
@@ -1,146 +1,108 @@
 #include <iostream>
-#include <locale>
 #include <vector>
+#include <locale>
 #include <memory>
+#include <limits>
 #include <unordered_map>
 #include <queue>
-#include <limits>
-#include <functional>
 #include <list>
+#include <functional>
 
-using std::cout, std::cin, std::locale;
+using std::cout, std::cin;
 
 struct Delimiter: std::ctype<char> {
-    static const mask* make_table(std::string ds) {
-        static std::vector<mask> v(classic_table(),
-                                   classic_table() + table_size);
-        for (auto mask: v) {
-            mask &= ~space;
+    static mask* make_table(const std::string& ds) {
+        static std::vector<mask> table(classic_table(),
+                                       classic_table() + table_size);
+        for (auto m: table) {
+            m &= ~space;
         }
         for (auto d: ds) {
-            v[d] |= space;
+            table[d] |= space;
         }
-        return &v[0];
+        return &table[0];
     }
-    Delimiter(std::string ds, size_t refs = 0):
+    Delimiter(const std::string& ds, size_t refs = 0):
         ctype(make_table(ds), false, refs) {}
 };
 
 struct Node;
 using NodePtr = std::shared_ptr<Node>;
-struct Aux {
-    NodePtr prev = nullptr;
-    int dist = std::numeric_limits<int>::max();
-    bool visited = false;
-};
+using VectorNodePtr = std::vector<NodePtr>;
+using ListNodePtr = std::list<NodePtr>;
 
 struct Node {
     std::string label;
-    Aux aux;
-    std::vector<NodePtr> adj;
-    Node(std::string label):
-        label(label){}
+    VectorNodePtr adjs;
+    NodePtr pred = nullptr;
+    int dist = std::numeric_limits<int>::max();
+    bool closed = false;
+    Node(const std::string label):
+        label(label) {}
 };
 
-std::ostream& operator<<(std::ostream& os, const Aux& aux) {
-    os << "(pred:" << (aux.prev ? aux.prev->label : "none")
-       << ", dist:" << aux.dist
-       << ", visi:" << aux.visited
-       << ")";
-    return os;
-}
+using Graph = std::unordered_map<std::string, NodePtr>;
+using WeightMap = std::unordered_map<std::string, std::unordered_map<std::string, int>>;
 
-std::ostream& operator<<(std::ostream& os, const NodePtr n) {
-    os << "  ("
-       << n->label
-       << ")[";
-    for (size_t i = 0; i < n->adj.size(); ++i) {
-        os << n->adj[i]->label;
-        if (i < n->adj.size() - 1) os << ", ";
-    }
-    os<< "]" << n->aux << "\n";
-    return os;
-}
-
-using NodeMap = std::unordered_map<std::string, NodePtr>;
-
-void addEntry(NodeMap& nodes,
-              const std::string& label) {
-    if (nodes.find(label) == nodes.end()) {
-        nodes[label] = std::make_shared<Node>(label);
+void addEntry(Graph& g, const std::string label) {
+    if (g.find(label) == g.end()) {
+        g[label] = std::make_shared<Node>(label);
     }
 }
 
-std::ostream& operator<<(std::ostream& os, const NodeMap& m) {
-    os << "{\n";
-    for (auto [key, value]: m) {
-        os << value;
-    }
-    os << "}";
-    return os;
-}
+using Cmp = std::function<bool(NodePtr a, NodePtr b)>;
+using PQueueNodePtr = std::priority_queue<NodePtr, VectorNodePtr, Cmp>;
 
-using Cmp = std::function<bool(NodePtr, NodePtr)>;
-using QNodePtr = std::priority_queue<NodePtr, std::vector<NodePtr>, Cmp>;
-using NodeMatrix = std::unordered_map<std::string, std::unordered_map<std::string, int>>;
-
-std::list<NodePtr> dijkstra(NodeMap& nodes,
-                              NodeMatrix& weights,
-                              const std::string& src,
-                              const std::string& dst) {
+ListNodePtr dijkstra(Graph& g,
+                     WeightMap& wm,
+                     const std::string& src,
+                     const std::string& dst) {
     Cmp cmp = [](NodePtr a, NodePtr b) {
-        return a->aux.dist > b->aux.dist;
+        return a->dist > b->dist;
     };
-    QNodePtr q(cmp);
-    std::list<NodePtr> result;
-    nodes[src]->aux.dist = 0;
-    q.push(nodes[src]);
+    PQueueNodePtr q(cmp);
+    g[src]->dist = 0;
+    q.push(g[src]);
     while (!q.empty()) {
         NodePtr u = q.top(); q.pop();
-        u->aux.visited = true;
-//        cout << "u:" << u << "\n";
-        for (auto v: u->adj) {
-            if (v->aux.visited) continue;
-            if (u->aux.dist + weights[u->label][v->label] < v->aux.dist) {
-                v->aux.dist = u->aux.dist + weights[u->label][v->label];
-                v->aux.prev = u;
+        for (const auto& v: u->adjs) {
+            if (u->dist + wm[u->label][v->label] < v->dist) {
+                v->dist = u->dist + wm[u->label][v->label];
+                v->pred = u;
                 q.push(v);
-//                cout << "relax:\n" << u << v << "\n";
             }
         }
     }
-    NodePtr aux = nodes[dst];
+    ListNodePtr result;
+    NodePtr aux = g[dst];
     while (aux != nullptr) {
         result.push_front(aux);
-        aux = aux->aux.prev;
-
+        aux = aux->pred;
     }
     return result;
 }
 
+
 int main(int argc, char** argv) {
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin.imbue(locale(cin.getloc(), new Delimiter(":")));
+    cin.imbue(std::locale(cin.getloc(), new Delimiter(":")));
+    Graph g; WeightMap wm;
     std::string src, dst;
     int weight;
-    NodeMap nodes;
-    NodeMatrix weights;
-    std::string s, d;
-    cin >> s >> d;
+    std::string from, to;
+    cin >> from >> to;
     while (cin >> src >> weight >> dst) {
-        addEntry(nodes, src);
-        addEntry(nodes, dst);
-        nodes[src]->adj.push_back(nodes[dst]);
-        weights[src][dst] = weight;
+        cout << src << dst << " = " << weight;
+        addEntry(g, src);
+        addEntry(g, dst);
+        g[src]->adjs.push_back(g[dst]);
+        wm[src][dst] = weight;
     }
-    std::list<NodePtr> path = dijkstra(nodes, weights, s, d);
+    ListNodePtr path = dijkstra(g, wm, from, to);
     size_t i = 0;
-    for (auto n: path) {
-        cout << n->label;
-        if (i++ < path.size() - 1) cout << "->";
+    for (const auto& p: path) {
+        cout << p->label;
+        if (i < path.size() - 1) cout << ", ";
     }
-    cout << " = " << nodes[dst]->aux.dist << '\n';
-//    cout << nodes << '\n';
+    cout << " = " << g[dst]->dist << "\n";
     return 0;
 }
